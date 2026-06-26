a372f925656f5fc939f127496a463b32c09bc50a
Deenasadhak <Deenasadhak.p.p@gmail.com>
2026-06-27 00:51:15 +0530

    #

---
+++ logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-06-26T17:34:21.384690700+00:00
+-- finished_at: 2026-06-26T17:34:26.605990200+00:00
+-- elapsed: 5.2s
+-- outcome: success
+-- dialect: bigquery
+-- node_id: not available
+-- query_id: g1z3bPndoM9zQ6Hfx25AZC8rXWr
+-- desc: not provided
+select 1 as id;
